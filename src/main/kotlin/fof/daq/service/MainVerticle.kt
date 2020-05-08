4f52b653119763c30eee45cd102d0d7b8784ab1b
max <592662285@qq.com>
2020-05-08 14:39:00 +0800

    MainVerticle 启动类 clear

--- src/main/kotlin/fof/daq/service/MainVerticle.kt
+++ src/main/kotlin/fof/daq/service/MainVerticle.kt
@@ -1,6 +1,5 @@
 package fof.daq.service
 
-import fof.daq.service.service.CarrierStatisticsService
 import fof.daq.service.web.WebVerticle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
@@ -12,29 +11,20 @@ import tech.kavi.vs.web.HandlerRequestAnnotationBeanName
 @ComponentScan(nameGenerator = HandlerRequestAnnotationBeanName::class)
 class MainVerticle : LauncherVerticle() {
 
-
     @Autowired
     private lateinit var webVerticle: WebVerticle
-    @Autowired
-    private lateinit var carrierStatisticsService: CarrierStatisticsService
 
     @Throws(Exception::class)
     override fun start() {
         super.start()
         vertx.deployVerticle(webVerticle)
-
-//        carrierStatisticsService.dataSelect("2020-05-05 00:00:00", "2020-05-08 00:00:00")?.subscribe({
-//            println("??????????????????????????????????")
-//            println( "=====================:$it" )
-//        },{it.printStackTrace()})
-
     }
 
     companion object {
         @JvmStatic
         fun main(args:Array<String>) {
             // 初始化类
-            launcher(fof.daq.service.MainVerticle::class.java)
+            launcher(MainVerticle::class.java)
         }
     }
 }

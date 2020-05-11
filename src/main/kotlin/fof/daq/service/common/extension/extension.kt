b974f8067e97f100c61ccbaec81bf6c4e8c51f34
max <592662285@qq.com>
2020-05-11 12:52:25 +0800

    日期格式正则 调整

--- src/main/kotlin/fof/daq/service/common/extension/extension.kt
+++ src/main/kotlin/fof/daq/service/common/extension/extension.kt
@@ -30,7 +30,7 @@ fun regexInt(num: String): Boolean {
  */
 fun regexDate(dateStr: String): Boolean {
 
-    var mainRegex = "\\d{4}(\\-|\\/|.)\\d{1,2}\\1\\d{1,2}\\s\\d{2}(:)\\d{2}(:)\\d{2}"
+    var mainRegex = "\\d{4}(\\-|\\/|.)\\d{2}\\1\\d{2}\\s\\d{2}(:)\\d{2}(:)\\d{2}"
     var p = Pattern.compile(mainRegex)
     val m = p.matcher(dateStr)
     return m.matches()
